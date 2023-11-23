cadd2fec40db91a6ed7b76b8c968c03d02ff24b3
rifqiwar <r.wariyanto@gmail.com>
2023-11-23 09:02:55 +0700

    style

--- resources/views/home/includes/style.blade.php
+++ resources/views/home/includes/style.blade.php
@@ -1,11 +1,13 @@
+<!-- Place favicon.png in the root directory -->
+<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+<!-- Font Icons css -->
+<link rel="stylesheet" href="{{url('themes-frontend/css/font-icons.css')}}">
+<!-- plugins css -->
+<link rel="stylesheet" href="{{url('themes-frontend/css/plugins.css')}}">
+<!-- Main Stylesheet -->
+<link rel="stylesheet" href="{{url('themes-frontend/css/style.css')}}">
+<!-- Responsive css -->
+<link rel="stylesheet" href="{{url('themes-frontend/css/responsive.css')}}">
 
-    <!-- Place favicon.png in the root directory -->
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
-    <!-- Font Icons css -->
-    <link rel="stylesheet" href="{{url('themes-frontend/css/font-icons.css')}}">
-    <!-- plugins css -->
-    <link rel="stylesheet" href="{{url('themes-frontend/css/plugins.css')}}">
-    <!-- Main Stylesheet -->
-    <link rel="stylesheet" href="{{url('themes-frontend/css/style.css')}}">
-    <!-- Responsive css -->
-    <link rel="stylesheet" href="{{url('themes-frontend/css/responsive.css')}}">
+<!-- load font awesome for icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
